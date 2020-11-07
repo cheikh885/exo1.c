77209b084f8ee276f262f9a2eb9467ecefe51332
cheikh885 <dcheikhballa9@gmail.com>
2020-11-07 23:25:37 +0100

    projet pour calculer produit en langage c

---
+++ exo1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int a, b, p;
+    
+    printf("veuillez saisir le 1er chiffre\n");
+    scanf("%d",&a);
+
+    printf("veuillez saisir le 2em chiffre\n");
+    scanf("%d",&b);
+
+p= a * b;
+{
+printf("la produit est %d\n",p);
+}
+return 0;
+}
---
+++ script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd Bureau/simplon
+touch fichier
+chmod u+x fichier
+mkdir -p sauvegarde
+cp -f fichier sauvegarde
